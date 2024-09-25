50afdf29e6c1e263161cf903aa0e7684dbfcf490
nekanediaz <116024637+nekanediaz@users.noreply.github.com>
2024-09-25 10:32:51 +0200

    Add files via upload
    subo thumb que ya funciona pero sin lr con tabulaciones ok chiii

--- practica1/matriz3x3_operar_THB.s
+++ practica1/matriz3x3_operar_THB.s
@@ -15,18 +15,18 @@ matriz3x3_operar_THB
 
 			; No hace falta inicializar Resultado ni E si solo vamos a hacer stores en memoria
 			; Movemos las direccion de las matrices a high registers
-			MOV 	R8, R0			   ; movemos @A a r8
-			MOV 	R9, R1 			   ; movemos @B a r9
-			MOV 	R10, R2 		   ; movemos @C a r10
-			MOV 	R11, R3 		   ; movemos @D a r11
+			MOV 	R8, R0				; movemos @A a r8
+			MOV 	R9, R1				; movemos @B a r9
+			MOV 	R10, R2				; movemos @C a r10
+			MOV 	R11, R3				; movemos @D a r11
 
 			; multiplicar(A,B,Resultado) and multiplicar(C,D,E)
 			
 			; for (i = 2; i >= 0; i--)
-			MOVS	R0, #2		; R6 = i = 2			
+			MOVS	R0, #2				; R6 = i = 2			
 ini_f_i_m	
 			; for (j = 2; j >= 0; j--)
-			MOVS	R1, #2		; R7 = j = 2
+			MOVS	R1, #2				; R7 = j = 2
 ini_f_j_m
 			; En r5 se va a guardar el resultado de cada Resultado[i][j]
 			MOVS 	R5, #0
@@ -108,10 +108,10 @@ ini_f_k_m
 			LSLS	R4, R0, #2
 			STR		R6, [R3, R4]
 
-			SUBS 	R1, R1, #1	; R1--
+			SUBS 	R1, R1, #1			; R1--
 			BPL		ini_f_j_m
 			; fin_for_j			
-			SUBS 	R0, R0, #1	; R0--	
+			SUBS 	R0, R0, #1			; R0--	
 			BPL		ini_f_i_m	
 			; fin_for_i
 
@@ -121,20 +121,20 @@ ini_f_k_m
 
 			; R12 = @Resultado 	SP = @E
 
-			MOVS 	R0, #9	; R0 = terminos_no_cero
-			MOVS	R1, #8	; R1 = i
-			MOV		R4, R12	; R4 = @Resultado
+			MOVS 	R0, #9				; R0 = terminos_no_cero
+			MOVS	R1, #8				; R1 = i
+			MOV		R4, R12				; R4 = @Resultado
 			MOV		R5, SP
 
 ini_f_suma	
 			LSLS	R6, R1, #2	
-			LDR 	R2, [R5, R6] 	; R5 = E[i][j]
-			LDR		R3, [R4, R6]	; R6 = Resultado[i][j]
+			LDR 	R2, [R5, R6]		; R5 = E[i][j]
+			LDR		R3, [R4, R6]		; R6 = Resultado[i][j]
 			ADDS	R3, R3, R2				
-			CMP 	R3, #0					; if (Resultado[i][j] == 0)
+			CMP 	R3, #0				; if (Resultado[i][j] == 0)
 			BNE		end_if
 			;IT EQ
-			SUBS	R0, R0, #1			    ; 		terminos_no_cero--
+			SUBS	R0, R0, #1			; terminos_no_cero--
 end_if		
 			STR 	R3, [R4, R6]
 
